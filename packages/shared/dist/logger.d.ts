23a62954cab3626e80a8450abe3820d7c13305e8
JacLight <imolewolede@gmail.com>
2025-07-26 20:14:24 -0500

    Refactor code structure and remove redundant changes

--- packages/shared/dist/logger.d.ts
+++
@@ -1,4 +0,0 @@
-import winston from 'winston';
-export declare const createLogger: (service: string) => winston.Logger;
-export declare const logger: winston.Logger;
-//# sourceMappingURL=logger.d.ts.map
